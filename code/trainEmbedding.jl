58c77108b766269f27ef9eb682dc515f8ef1daee
Arcumenn <steinar.graessel@gmx.net>
2021-10-05 20:23:04 +0200

    add more converted methods

--- code/trainEmbedding.jl
+++ code/trainEmbedding.jl
@@ -1,5 +1,6 @@
 using DelimitedFiles
-using Revise
+using Flux
+using Dates
 
 const MAX_LENGTH = 25
 const N_TRAINING = 2000
@@ -115,6 +116,81 @@ function filterPair(p::SubArray)::Bool
 end # filterPair
 
 
-input, output, pairs = prepareData("asjpIn", "asjpOut", false)
-word2index_dict = sort(collect(input.word2index), by=x->x[2])
+input_lang, output_lang, pairs = prepareData("asjpIn", "asjpOut", false)
+word2index_dict = sort(collect(input_lang.word2index), by=x->x[2])
 show(word2index_dict)
+
+
+function vectorsFromPair(pair::Vector{String})::Tuple{Vector{Int64}, Vector{Int64}}
+    input_vec = vectorsFromSentence(input_lang, pair[1])
+    target_vec = vectorsFromSentence(output_lang, pair[2])
+    return input_vec, target_vec
+end # vectorsFromPair
+
+
+function vectorsFromSentence(lang::Lang, sentence::String)::Vector{Int64}
+    indeces = [lang.word2index[word] for word in split(sentence)]
+    append!(indeces, EOS_token)
+    return indeces
+end # vectorsFromSentence
+
+
+### Python Beginning ###
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+### Python End ###
+
+
+ 
+
+encoderRNN = Chain(Flux.Embedding(input_lang.n_words, hidden_size), 
+                   GRU(hidden_size, hidden_size))
+
+
+function trainIters(encoder, decoder, n_iters, print_every=1000, learning_rate=0.01)
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0   # Reset every plot_every
+
+    # TO DO
+    # encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    # decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_pairs = [vectorsFromPair(rand(pairs[1:N_TRAINING])) for i in 1:n_iters]
+    # criterion = nn.NLLLoss()
+    p = Progress(Int(floor(n_iters / print_every)), showspeed=true)
+    for iter in 1:n_iters
+        sleep(0.1)
+        training_pair = training_pairs[iter]
+        input_tensor = training_pair[1]
+        target_tensor = training_pair[2]
+
+        # loss = train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if iter % print_every == 0
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            next!(p; showvalues = [(:Iteration, iter), (:LossAverage, print_loss_avg)])
+        end # if
+    end # for
+end # trainIters
+
